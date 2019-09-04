afe8eab26872e5f6a2f6753aa2a14b4e4c182aca
Torsten Sommer <torsten.sommer@3ds.com>
2019-09-04 12:38:35 +0200

    Fix assertion in test case

--- C/tests/ExternalLibrary_test.cpp
+++ C/tests/ExternalLibrary_test.cpp
@@ -57,7 +57,7 @@ TEST_CASE("External functions can be loaded and called", "[ExternalLibrary]") {
 
 		auto message = fp_externalFunction(DATA_FILE, MODULE_NAME, FUNCTION_NAME, PYTHON_HOME, 2, u, 2, y);
 
-		CHECK(message == nullptr);
+		CHECK_THAT(message, Equals(""));
 		CHECK(y[0] == 4);
 		CHECK(y[1] == 5);
 	}
